b9f700cf6e62391c6fecbc9477348383536b7d0f
renzoflores <rdflores07@gmail.com>
2020-06-12 11:26:56 -0300

    open page code

--- scrape_houses.py
+++ scrape_houses.py
@@ -3,4 +3,12 @@ import urllib3
 from bs4 import BeautifulSoup
 import pandas as pd
 
-#
+# URL pages
+page = 'https://www.point2homes.com/CA/Real-Estate-Listings/NB.html'
+
+http = urllib3.PoolManager()
+req = http.request('GET',page)
+
+
+soup = BeautifulSoup(req.data, 'html.parser')
+print(soup)
