b10f2a3baaa44d53bc436664d573a04fb7163720
renzoflores <rdflores07@gmail.com>
2020-06-12 15:30:13 -0300

    Simplified code to get all houses for sale

--- merge_csv.py
+++
@@ -1,8 +0,0 @@
-
-import pandas as pd
-
-df1 = pd.read_csv("houseprices_nb.csv")
-df2 = pd.read_csv("houseprices_nb1.csv")
-
-final_df = pd.concat([df1,df2],ignore_index=True)
-final_df.to_csv("houseprices_nb_merged.csv",index=False)
